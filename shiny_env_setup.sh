21c096dffea18818194e12c3ff19df7fd38d6deb
Connor Krenzer <crkrenz72@gmail.com>
2024-01-01 23:07:15 -0500

    specifying the exact files the shiny app needs

--- shiny_env_setup.sh
+++ shiny_env_setup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
 mkdir -p wordle_assistant/data
+mkdir -p wordle_assistant/r
 cp app.R wordle_assistant/
+cp r/play.R wordle_assistant/r
 cp data/opening_word_scores.csv wordle_assistant/data/
 cp data/unigram_freq.csv wordle_assistant/data/
 cp data/wordle_list.txt wordle_assistant/data/
-cp --recursive r/ wordle_assistant
 
 Rscript deploy.R
